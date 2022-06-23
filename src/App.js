4dcbde05bbfc4f391d86829f3538a3974901860c
AlisonTan <alisontan97@yahoo.com>
2022-06-23 18:47:47 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import quoteArray from './collection _of_quotes.js'
+import {ReactComponent as ManChilling} from './ManChilling.svg';
+import {ReactComponent as ShakingLeg} from './leg.svg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+var colors = [
+  '#864F89', '#385723', '#006AA3', '#941100', '#ED8E36', '#00A88E', '#798DA5'
+]
+let colorIndex = 1;
+
+class App extends React.Component {
+  state = {
+    quote: "I'm here to cool myself down",
+    author: "Yourself",
+    color: '#864F89'
+  }
+
+
+  
+  getRandomIndex = () => {
+    const index = Math.floor(Math.random() * quoteArray.length);
+    this.setState({
+      quote: quoteArray[index].quote,
+      author: quoteArray[index].author,
+    })
+    this.setState({
+      color: colors[colorIndex]
+    })
+    if(colorIndex === colors.length - 1) {
+      colorIndex = 0
+    } else colorIndex++
+  }
+
+  render(){
+    return (
+      <div className="App"  >
+        <div id="background" style={ { background: `${ this.state.color }` } }></div>
+        
+        <div id="canvas">   
+          <ManChilling fill={this.state.color} />
+          <img src="/picture/typingHand.svg" alt="" id="typingHand"></img>
+          <ShakingLeg fill={this.state.color} id="shakingLeg"/>
+
+        </div>
+        <div id="quote-box" style={{color: `${ this.state.color }`}}>
+          <p id='text'>{this.state.quote}</p>
+          <p id='author'>- {this.state.author}</p>
+        </div>
+        <button id="next-button" onClick={this.getRandomIndex} style={ { background: `${ this.state.color }` } }>NEXT</button>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/collection _of_quotes.js
@@ -0,0 +1,35 @@
+let quoteArray =
+[
+  {quote:'Anger makes you smaller, while forgiveness forces you to grow beyond what you were.',
+    author:'Chérie Carter-Scott'},
+  {quote:'Fear is the path to the dark side. Fear leads to anger. Anger leads to hate. Hate leads to suffering.',
+    author:'Yoda'},
+  {quote:'Do not let your anger lead to hatred, as you will hurt yourself more than you would the other',
+    author:'Stephen Richards'},
+  {quote:"'I lose my temper, but it's all over in a minute,' said the student. 'So is the hydrogen bomb,' I replied. 'But think of the damage it produces!'",
+    author:'George Sweeting'},
+  {quote:"There's nothing wrong with anger provided you use it constructively.",
+    author:'Wayne Dyer'},
+  {quote:'The best fighter is never angry.',
+    author:'Lao Tzu'},
+  {quote:'When angry count to ten before you speak. If very angry, count to one hundred.',
+    author:'Thomas Jefferson'},
+  {quote:"Never respond to an angry person with a fiery comeback, even if he deserves it... Don't allow his anger to become your anger.",
+    author:'Bohdi Sanders'},
+  {quote:'Never do anything when you are in a temper, for you will do everything wrong.',
+    author:'Baltasar Gracian'},
+  {quote:'If you are patient in one moment of anger, you will escape a hundred days of sorrow.',
+    author:'Chinese Proverb'},
+  {quote:'Holding on to anger is like grasping a hot coal with the intent of throwing it at someone else; you are the one who gets burned.',
+    author:'Buddha'},
+  {quote:"Anger is stupid, and stupidity will kill you more surely than your opponent's blade.",
+    author:'Patricia Briggs'},
+  {quote:"Don't waste your time in anger, regrets, worries, and grudges. Life is too short to be unhappy.",
+    author:'Roy T. Bennett'},
+  {quote:"It is wise to direct your anger towards problems - not people; to focus your energies on answers - not excuses.",
+    author:'William Arthur Ward'}
+]
+
+  // source: https://kidadl.com/quotes/best-anger-management-quotes-to-keep-calm
+
+  export default quoteArray
