c1578b9c67ad79c89aa4272605f02677e2ca3b0b
AlisonTan <alisontan97@yahoo.com>
2022-07-15 13:52:39 +0800

    fixed animation (typing hand)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import "./App.css";
 import React from "react";
-import quoteArray from "./collection _of_quotes.js";
-import { ReactComponent as ManChilling } from "./ManChilling.svg";
-import { ReactComponent as ShakingLeg } from "./leg.svg";
-import typingHand from './TypingHand.svg';
+import { ReactComponent as ManChilling } from "./SVG/ManChilling.svg";
+import { ReactComponent as ShakingLeg } from "./SVG/leg.svg";
+import { ReactComponent as MusicNote } from "./SVG/music_note_1.svg";
+import typingHand from './SVG/TypingHand.svg';
 
 var colors = [
   "#864F89",
@@ -16,45 +16,59 @@ var colors = [
 ];
 let colorIndex = 1;
 
+const quoteAPI = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
+
 class App extends React.Component {
   state = {
-    quote:
-      "It is wise to direct your anger towards problems - not people; to focus your energies on answers - not excuses.",
-    author: "William Arthur Ward",
+    quotes: [{quote:'',author:''}],
+    index: 0,
     color: "#864F89",
   };
 
+  componentDidMount() {
+    fetch(quoteAPI).then(res => res.json())
+      .then(res => {
+        this.setState({
+          quotes: res.quotes
+        }, this.getRandomIndex);
+    });
+  }
+
   getRandomIndex = () => {
-    const index = Math.floor(Math.random() * quoteArray.length);
+    const { quotes } = this.state;
+    const randomIndex = Math.floor(Math.random() * quotes.length);
     this.setState({
-      quote: quoteArray[index].quote,
-      author: quoteArray[index].author,
-    });
-    this.setState({
-      color: colors[colorIndex],
-    });
+      index: randomIndex,
+      color: colors[colorIndex]
+    })
+
     if (colorIndex === colors.length - 1) {
       colorIndex = 0;
     } else colorIndex++;
   };
 
   render() {
-    const {color, quote, author} = this.state
+    const {color, quotes, index} = this.state
+    const singleQuote = quotes[index];
     return (
       <div className="App">
         <div id="background" style={{ background: `${color}` }} ></div>
 
+        {/* CANVAS */}
         <div id="canvas">
-          <ManChilling fill={color} />
+          <ManChilling fill={color} id="manChilling" />
           <ShakingLeg fill={color} id="shakingLeg" />
           <img src={typingHand} alt="" id="typingHand"></img>
-
-
+          <MusicNote fill={color} id="musicNote1" />
         </div>
+
+
         <div id="quote-box" style={{ color: `${color}` }}>
-          <p id="text">{quote}</p>
-          <p id="author">- {author}</p>
+          <p id="text">{singleQuote.quote}</p>
+          <p id="author">- {singleQuote.author}</p>
         </div>
+
+
         <button
           id="next-button"
           onClick={this.getRandomIndex}
